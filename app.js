e3d569ea608419901a3cfd4775b2466ac4681a06
Kershan Naidoo <n.kershan@gmail.com>
2020-04-08 23:40:07 +0100

    Add key events manager and refactor paddle vars

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-
 var canvas;
 var ctx;
+
+//Paddle
+var paddleWidth = 80;
+var paddleX;
+var paddleY;
 
 window.onload = function () {
     canvas = document.getElementById('canvas');
@@ -8,12 +12,37 @@ window.onload = function () {
 
     setNewScreen();
 
+    addEventListener("keydown", keyDownEvent);
+
     requestAnimationFrame(gameUpdate);
 }
 
 function setNewScreen() {
+    paddleX = canvas.width / 2 - (paddleWidth / 2);
+    paddleY = canvas.height - 20;
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function keyDownEvent(event) {
+    if (event.isComposing || event.keyCode === 229) {
+        return;
+    }
+
+    switch(event.keyCode) {
+        case 37: {
+            //Left
+        }
+        case 38: {
+            //Up
+        }
+        case 39: {
+            //Right
+        }
+        case 40: {
+            //Down
+        }
+    }
 }
 
 function gameUpdate() {
@@ -23,6 +52,6 @@ function gameUpdate() {
 
 function drawPaddle() {
     ctx.fillStyle = 'green';
-    ctx.fillRect(canvas.width / 2 - 40, canvas.height - 20, 80, 20);
+    ctx.fillRect(paddleX, paddleY, paddleWidth, 20);
 }
 
